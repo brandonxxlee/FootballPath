4cc87aa981b710676e74b61f03aef28ef811d723
Sirjan Kafle <sxk1693@berkeley.edu>
2016-10-01 11:19:08 -0700

    Add helper method to read in image

--- photoDetection.py
+++ photoDetection.py
@@ -1,14 +1,21 @@
 import scipy.misc as sp
 
-def pixelAtPosition(picture, row, col):
-    return picture[row][col]
-imageName = "gradientBlack.jpg"
-im = sp.imread(imageName)
+IMAGE_FILE_PATH = './images/'
 
+
+def pixel_at_position(picture, row, col):
+    return picture[row][col]
+
+
+def read_in_image(file_name):
+    return sp.imread(file_name)
+
+image_name = "gradientBlack.jpg"
+im = read_in_image(image_name)
 
 # Do stuff to im
-im2 = sp.imresize(im, 5 )
-
+im2 = sp.imresize(im, 5)
 
 
 sp.toimage(im2, cmin=0.0, cmax=...).save("next.jpg")
+
