e88723e6b077ce916aa95216eb45a948a24e9544
Brandon Lee <brandonxxlee@gmail.com>
2016-10-01 18:15:20 -0700

    added edge_filter line

--- photoDetection.py
+++ photoDetection.py
@@ -2,13 +2,13 @@ import scipy.misc as sp
 
 def pixelAtPosition(picture, row, col):
     return picture[row][col]
-imageName = "gradientBlack.jpg"
-im = sp.imread(imageName)
+imageName = "images/footballPlayer.jpg"
+im = sp.imread(imageName, flatten=True)
 
+print (im)
+print (type(im))	
 
-# Do stuff to im
-im2 = sp.imresize(im, 5 )
-
-
-
+im2 = sp.imfilter(im, "find_edges")
 sp.toimage(im2, cmin=0.0, cmax=...).save("next.jpg")
+
+
