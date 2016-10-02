460bd3f2aec6e8a34fbfadd779dea8cd3ddd32c1
Brandon Lee <brandonxxlee@gmail.com>
2016-10-02 03:50:11 -0700

    added initial values for the water bottle

--- videoParse.py
+++ videoParse.py
@@ -4,15 +4,7 @@ import argparse
 import sys
 import time
 
-import photoDetection
-import meanShift
-import rectangle_finder
-
-frame = None
-roiPts = []
-
 def main():
-	global frame, roiPts
 	args_parser = argparse.ArgumentParser()
 	args_parser.add_argument("-v", "--video", help = "path  to the video file")
 	args = vars(args_parser.parse_args())
@@ -25,11 +17,12 @@ def main():
 
 	cv2.namedWindow("frame")
 
-	termination = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT ,10, 1)
+	termination = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT , 10, 1)
 	roiBox = None
 
 	while True:
 		(grabbed, frame) = camera.read()
+		time.sleep(.1)
 
 		if not grabbed:
 			break
@@ -43,16 +36,17 @@ def main():
 			cv2.polylines(frame, [pts], True, (0, 255, 0), 2)
 
 		cv2.imshow("frame", frame)
-
+		cv2.waitKey(1)
 		#Create initial rectangle frame
-		if not roiPts:
-			oirg = frame.copy()
+		if roiBox is None:
+			print("TEST")
+			orig = frame.copy()
 
-			tl = [0,0] #TODO: Top Left of rectangle
-			br = [0,0] #TODO: Bottom Right of rectangle
+			tl = [50,50] #TODO: Top Left of rectangle
+			br = [100,500] #TODO: Bottom Right of rectangle
 
-			roi = orig[t1[1]:br[1], t1[0]:br[0]]
-			roi = cv2.cvtColor(coi, cv2.COLOR_BGR2HSV)
+			roi = orig[tl[1]:br[1], tl[0]:br[0]]
+			roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
 
 			roiHist = cv2.calcHist([roi], [0], None, [16], [0, 180])
 			roiHist = cv2.normalize(roiHist, roiHist, 0, 255, cv2.NORM_MINMAX)
