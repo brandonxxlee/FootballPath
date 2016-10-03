c25fcf6686bae996f1c061b66abb84da5bafc593
jozhang97 <jozhang@berkeley.edu>
2016-10-03 00:10:54 -0700

    Updated project description

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-# FootballPath
+# FootballPath / ViDraw
 SD Hacks 2016
 
+Given a football video stream, track the receiver and draw his path. 
+
+##Our algorithm
+1. Run k-means on RGB to separate green grass and colored players
+2. Run k-means on spatial coordinate to distinguish players 
+3. Draw rectangles around them
+4. Use Camshift to track movements through time in video (need OpenCV library)
+5. Connect the dots and formuate the path 
 
 Setting up OpenCV: http://www.pyimagesearch.com/2015/06/29/install-opencv-3-0-and-python-3-4-on-osx/
--- generate.py
+++ generate.py
@@ -18,14 +18,15 @@ center_colors, im2, white_colors = photoDetection.k_means_teams(im, 10)
 rects = rectangle_finder.find_rectangles(center_colors, im2, im)
 image = Image.open("images/" + img)
 rects = rectangle_finder.scaleUp(rects, resizeFactor)
+rects = rects[len(rects)//2:]
 images_with_rectangles = rectangle_finder.draw_rectangles(rects, image)
 
-pos = []
-for i in im2:
-    #plotter.plot(i)
-    pos.append(rectangle_finder.find_rectangles_for_given_locations(i))
-pos = rectangle_finder.scaleUp(pos, resizeFactor)
-images_with_rectangles = rectangle_finder.draw_rectangles(pos, images_with_rectangles)
+# pos = []
+# for i in im2:
+#     #plotter.plot(i)
+#     pos.append(rectangle_finder.find_rectangles_for_given_locations(i))
+# pos = rectangle_finder.scaleUp(pos, resizeFactor)
+# images_with_rectangles = rectangle_finder.draw_rectangles(pos, images_with_rectangles)
 
 meanShift.save_image(images_with_rectangles)
 
